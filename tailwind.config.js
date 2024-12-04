50495d3b37c023d1afdbd59d8bdd511021982ad0
fb.lb <fb.lubre@free.fr>
2024-12-04 16:51:28 +0100

    Add Tailwind, prettier-plugin-tailwindcss and header component

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}",],
+  theme: {
+    screens: {
+      'medium': '769px',
+      'large': '993px'
+    },
+    fontFamily: {
+      'graphik': ['Graphik', 'Arial', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'primary': '#f1f8fc',
+        'secondary': '#0074c7',
+        'tertiray': '#00497c',
+        'dark': '#384050',
+        'success': '#82b864',
+        'danger': '#cd2c2e'
+      }
+    },
+  },
+  plugins: [],
+}
+
